2060ee9d0dfa1d98fca4574d55489cc981ffe227
Sikstep <Sikstep1988@gmail.com>
2023-02-13 18:02:00 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,36 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+let tags:any = [
+    // {
+    //     id: 1, title: 'CSS',
+    // },
+    // {
+    //     id: 2, title: 'js',
+    // },
+    // {
+    //     id: 3, title: 'react',
+    // },
+    // {
+    //     id: 4, title: 'html',
+    // },
+    // {
+    //     id: 5, title: 'java',
+    // },
+]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+   <div>
+       <div>
+           {
+               tags.map((el: any) => {
+                   return <div key={el.id}>{el.title}</div>
+               })
+           }
+
+       </div>
+   </div>
   )
 }
 
